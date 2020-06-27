a8a2b4f279871b0473f4f853362308d61f110212
Rahul Singh <rscodedocs@gmail.com>
2020-06-27 19:38:08 +0530

    initial setup with typescript and tailwind css

---
+++ pages/Colors.tsx
@@ -0,0 +1,34 @@
+const colorList = [ 'gray', 'red', 'orange', 'yellow', 'green', 'teal', 'indigo', 'blue', 'purple', 'pink' ];
+
+const shades = [ 100, 200, 300, 400, 500, 600, 700, 800, 900 ];
+
+const colors = () => {
+	return (
+		<div className='flex  mt-8 flex-col'>
+			<h1 className='text-xl font-bold mb-4 pb-2 border-gray-500 border-b-2'>
+				{'colors with no shades'.toUpperCase()}
+			</h1>
+			<div className='flex mb-8'>
+				<div className='w-16 h-16 bg-black' />
+				<div className='w-16 h-16 bg-white border-2 border-gray-500 ml-4' />
+			</div>
+			<h1 className='text-xl font-bold mb-4 pb-2 border-gray-500 border-b-2'>
+				{'colors with shades'.toUpperCase()}
+			</h1>
+			<div className='flex justify-between mt-4'>
+				{colorList.map((color, i) => (
+					<div key={i}>
+						<h1 className='text-xl font-semibold mb-4'>{color.toLowerCase()}</h1>
+						{shades.map((shade, j) => (
+							<div key={j} className={`w-16 h-16 bg-${color}-${shade} flex justify-center items-center`}>
+								{shade}
+							</div>
+						))}
+					</div>
+				))}
+			</div>
+		</div>
+	);
+};
+
+export default colors;
---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import App from 'next/app';
+
+import '../styles/tailwind.css';
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+    return <Component {...pageProps} />;
+  }
+}
+
+export default MyApp;
